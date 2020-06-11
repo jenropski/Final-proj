a43dc3173d5f7a348e0933228b5dd5991d729cb3
jenropski <63664056+jenropski@users.noreply.github.com>
2020-06-11 00:42:28 -0400

    everything on git
    ya

---
+++ dev/js/scripts.js
@@ -0,0 +1,24 @@
+var $burger = $("#burger-icon");
+var $nav = $('nav[data-nav]');
+var $navA = $('nav[data-nav] a');
+var isVisible = false;
+ 
+function mouseClick(){
+ 
+if($(window).width() <= 992){
+console.log("click");
+if(isVisible === false){
+$nav.show();
+isVisible = true;
+}
+else{
+$nav.hide();
+isVisible = false;
+}
+}
+}
+ 
+$burger.on( "click", mouseClick);
+// close the mobile menu when menu link is clicked
+$navA.on( "click", mouseClick);
+
